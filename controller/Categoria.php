001d56655141802a0f1990cdc96c76ac9cec42c8
JosÃ© Roberto Pereira Neto <13372832663@dc01.ifs.ifsuldeminas.edu.br>
2022-06-29 16:56:05 -0300

    front-controller

--- controller/Categoria.php
+++ controller/Categoria.php
@@ -1,9 +1,28 @@
 <?php
-require "../model/CategoriaModel.php";
+require "model/CategoriaModel.php";
 
-$categoria = new CategoriaModel();
-$categoria->inserir("SmartTV");
-$categoria->excluir(12);
-$categoria->atualizar(6, "SmartPhone");
-var_dump($categoria->buscarPorId(6));
-var_dump($categoria->buscarTudo());
+class Categoria{
+
+    function __construct() {
+        $this->modelo = new CategoriaModel();
+    }
+
+    function index(){
+       include "view/template/conteudo.php";
+    }
+
+    function add(){
+        echo "mostrar form categoria";
+    }
+
+    function excluir($id){
+        echo "excluir categoria";
+    }
+}
+
+//$categoria = new CategoriaModel();
+//$categoria->inserir("SmartTV");
+//$categoria->excluir(12);
+//$categoria->atualizar(6, "SmartPhone");
+//var_dump($categoria->buscarPorId(6));
+//var_dump($categoria->buscarTudo());
